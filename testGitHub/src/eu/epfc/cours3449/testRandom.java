b1d2836a5b0cbed2e1e6412d43868bef6fdfebc2
2512lalartigue <2512lalartigue@PC8831.domepfc.net>
2015-12-01 20:49:07 +0100

    second testRandom

--- testGitHub/src/eu/epfc/cours3449/testRandom.java
+++ testGitHub/src/eu/epfc/cours3449/testRandom.java
@@ -10,14 +10,12 @@ import java.util.Random;
 
 public class testRandom {
     public static void main(String[] args) {
-        Random r1 = new Random(3);
-        for(int i = 0; i < 10 ; i++){
-            System.out.print(r1.nextInt(1000) + "  ");
-        }
-        System.out.println("");
-        Random r2 = new Random(4); //si paramètre identique => Random identique!!!
-        for(int i = 0; i < 10 ; i++){
-            System.out.print(r2.nextInt(1000) + "  ");
+        for (int j = 0; j < 2; j++) {
+            Random r1 = new Random(3);
+            for (int i = 0; i < 10; i++) {
+                System.out.print(r1.nextInt(1000) + "  ");
+            }
+            System.out.println("");
         }
     }
 }
