8f00dc7508543a4e277e057c6131eb8e232f3801
2512lalartigue <2512lalartigue@PC8831.domepfc.net>
2015-12-01 19:36:17 +0100

    first change "Welcome"

--- testGitHub/src/eu/epfc/cours3449/TestGitHub.java
+++ testGitHub/src/eu/epfc/cours3449/TestGitHub.java
@@ -16,7 +16,7 @@ public class TestGitHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Welcome GitHub");
     }
     
 }
