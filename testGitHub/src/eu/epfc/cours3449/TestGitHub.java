bb08ca5398002f7b92cde5d8723a784c707638f5
2512lalartigue <2512lalartigue@PC8831.domepfc.net>
2015-12-01 19:18:54 +0100

    Create first class in repository

---
+++ testGitHub/src/eu/epfc/cours3449/TestGitHub.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eu.epfc.cours3449;
+
+/**
+ *
+ * @author 2512lalartigue
+ */
+public class TestGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
